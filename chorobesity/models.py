39e452735957ee08243862f2e6c5505e5667781d
Bradley Freedman <bradleytfreedman@gmail.com>
2023-11-13 05:24:53 -0500

    Added new statistic fields

--- chorobesity/models.py
+++ chorobesity/models.py
@@ -8,6 +8,10 @@ class State(models.Model):
     obesity_population_afflicted = models.IntegerField(null=True, blank=True, default=0)
     diabetes_percentage_afflicted = models.IntegerField(null=True, blank=True, default=0)
     diabetes_population_afflicted = models.IntegerField(null=True, blank=True, default=0)
+    mean_obesity_percentage = models.DecimalField(null=True, max_digits=10, decimal_places=2, blank=True, default=0)
+    mean_diabetes_percentage = models.DecimalField(null=True, max_digits=10, decimal_places=2, blank=True, default=0)
+    std_obesity_percentage = models.DecimalField(null=True, max_digits=10, decimal_places=2, blank=True, default=0)
+    std_diabetes_percentage = models.DecimalField(null=True, max_digits=10, decimal_places=2, blank=True, default=0)
 
 class County(models.Model):
     id = models.CharField(primary_key=True, max_length=55, null=False, blank=False, default="")
