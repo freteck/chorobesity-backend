8d99187458d185d48d181db314f4bb4b20dfdcd3
Bradley Freedman <bradleytfreedman@gmail.com>
2023-11-30 14:20:30 -0500

    Deployment improvements

--- chorobesity_api/urls.py
+++ chorobesity_api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 from chorobesity import views
+from django.views.generic.base import RedirectView
 
 router = routers.DefaultRouter()
 router.register(r'states', views.StateListViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('', RedirectView.as_view(url='/api/states', permanent=False), name='index')
 ]
